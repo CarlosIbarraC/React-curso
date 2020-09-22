cea9729f16c1d0bf4f2fc3ddc10927bab4cd7b92
CarlosIbarraC <ceibarra1@gmail.com>
2020-09-21 21:19:51 -0500

    seccion4

---
+++ src/CounterApp.js
@@ -0,0 +1,30 @@
+import React,{useState} from 'react';
+import PropTypes from 'prop-types'; 
+
+const CounterApp = ({value=10})=>{
+     
+    const [counter, setCounter] = useState(value);
+
+    
+
+    const handleAdd = (e)=>{
+        setCounter(counter +1);
+        //setCounter((c)=>c+1)
+    }
+    const handleDis = (e)=>{setCounter(counter -1)}
+    const handleDef = (e)=>{setCounter(value)}
+    return(<>
+    <h1>CounterApp</h1>
+    <p>{counter}</p>
+    <button onClick={handleAdd}>+1</button>
+    <button onClick={handleDef}>Reset</button>
+    <button onClick={handleDis}>-1</button>
+    </>
+    ) 
+}
+CounterApp.prototype={
+    value:PropTypes.number.isRequired
+}
+export default CounterApp;
+
+
---
+++ src/PrimeraApp.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types'; 
+
+
+
+
+ const PrimeraApp = ({saludo,subtitulo })=>{
+
+  
+
+  return (<>
+          
+        <h1>{saludo}</h1>
+
+      {/* <pre>{JSON.stringify(saludo,null,3)}</pre> */}
+        <p>{subtitulo}</p>
+  </>
+    );
+ }
+ PrimeraApp.propTypes = {
+   saludo: PropTypes.string.isRequired
+ }
+
+ PrimeraApp.defaultProps = {
+   subtitulo:'soy un subtitulo'
+ }
+
+ export default PrimeraApp;
